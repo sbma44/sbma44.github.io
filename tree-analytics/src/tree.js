9674208b00bbe9239c67e6b3cf3060e118fe342c
Tom Lee <thomas.j.lee@gmail.com>
2024-12-08 12:59:58 -0500

    update to reflect new CSV format

--- tree-analytics/src/tree.js
+++ tree-analytics/src/tree.js
@@ -6,20 +6,19 @@ xhr.responseType = "text";
 xhr.onload = function() {
 if (xhr.status === 200) {
     let maxDist = 0;
-    const filterDate = new Date('2023-12-16 23:00:00');
     const csvData = this.response
         .split("\n").slice(1)
         .map((row) => {
             const rowParts = row.split(',');
             const r = [
-                new Date(rowParts[0]),
+                new Date(parseInt(rowParts[0]) * 1000),
                 rowParts[rowParts.length - 1].trim().length > 0 ? -1 * parseFloat(rowParts[rowParts.length - 1]) : false
             ];
             if (r[1] && (r[1] > maxDist))
                 maxDist = r[1];
             return r;
         })
-        .filter(row => (!!row[1]) && (row[0] > filterDate));
+        .filter(row => !!row[1]);
 
     new Dygraph(
         document.getElementById("darkbg"),
